1321932bb252996a98dad001d2d6379b0756205a
David Byrd <david@byrdhou.se>
2013-03-30 01:34:09 -0400

    added prefetch request limit

--- preempt.js
+++ preempt.js
@@ -4,29 +4,26 @@ $(document).ready(function () {
   console.log('preempt loaded.');
 
   var store = {},
-      storeSize = 0;
+      storeSize = 0,
+      links = $('a'),
+      maxSize = Math.min(20, Math.floor(0.3*links.length)),
+      fetch = function (idx, a) {
+        if (!store[a.href])
+          $.get(a.href, function (html) {
+            var pattern = /<html[^>]*>((.|[\n\r])*)<\/html>/im.exec(html)
+            if (pattern && pattern.length > 0 && storeSize++ < maxSize) store[a.href] = pattern[0]
+          })
+      },
+      swap = function (e) {
+        var cachedPage = store[e.target.href]
+        if (cachedPage) {
+          $('html').html(cachedPage)
+          history.pushState({ foo: "bar"}, "ignored", e.target.href)
+          return false
+        }
+      }
 
-  $.each($('a'), function (idx, a) {
-
-    if (!store[a.href]) {
-
-      $.get(a.href, function (html) {
-        var pattern = /<html[^>]*>((.|[\n\r])*)<\/html>/im.exec(html)
-        if (pattern && pattern.length > 0 && ++storeSize < 10) store[a.href] = pattern[0]
-        console.log('cached ' + a.href + '.')
-      })
-    
-    }
-  })
-
-
-  $(document).on('click', 'a', function (e) {
-    var cachedPage = store[e.target.href]
-    if (cachedPage) {
-      $('html').html(cachedPage)
-      history.pushState({ foo: "bar"}, "ignored", e.target.href)
-      return false
-    }
-  })
+  $.each(links, fetch) //prefetch
+  $(document).on('click', 'a', swap) //swap cached version
 
 })
